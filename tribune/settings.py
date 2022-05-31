337be516df612a7e5c7a5b173b2dacc576fcb2c8
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-05-31 11:11:30 +0300

    deployment

--- tribune/settings.py
+++ tribune/settings.py
@@ -16,7 +16,7 @@ from decouple import config,Csv
 
 import django_heroku
 
-MODE=config("MODE", default="dev")
+MODE=config("MODE", default="prod")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
